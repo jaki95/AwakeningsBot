9fe5392936ce9b967dec04e16a0f8e996c64ad6a
jaki95 <jacopocarrani@gmail.com>
2022-07-11 17:10:35 +0100

    env variable format

--- src/scheduler.py
+++ src/scheduler.py
@@ -7,9 +7,9 @@ chat_id = 1317888738
 scrape_interval = os.environ.get('AWK-SCRAPE')
 
 # Switch between every 1 and every 60 minute scraping
-if scrape_interval == 1:
+if scrape_interval == "1":
     schedule.every().minute.at(":01").do(event.check_events)
-if scrape_interval == 60:
+if scrape_interval == "60":
     schedule.every().hour.at(":01").do(event.check_events)
 
 while True:
